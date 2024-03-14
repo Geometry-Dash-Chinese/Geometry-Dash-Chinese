13a8556294b38ada29de25329589c37a0a444196
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-03-14 10:17:40 +0800

    chore(frontend): apply path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import laravel from 'laravel-vite-plugin'
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, 'src')
+		}
+	},
 	plugins: [
 		laravel({
 			input: ['resources/css/app.css', 'resources/js/app.js'],
