bc6eb5ac134b9234d2881f682c912277a595b101
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-03-25 14:50:44 +0800

    feat(GeometryDashProxy): add account data proxy routes

--- routes/GeometryDashProxy.php
+++ routes/GeometryDashProxy.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GeometryDashProxyAccountDataController;
 use App\Http\Controllers\GeometryDashProxyController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,7 +13,22 @@ Route::group([
 	Route::group([
 		'excluded_middleware' => ['web']
 	], function () {
-		Route::post('/{path}', [GeometryDashProxyController::class, 'proxy'])->name('proxy');
+		Route::post('/getAccountURL.php', [GeometryDashProxyAccountDataController::class, 'getURL'])->name('account.url.get');
+
+		Route::group([
+			'prefix' => '/accounts',
+			'as' => 'account.'
+		], function () {
+			Route::group([
+				'prefix' => '/data',
+				'as' => 'data.'
+			], function () {
+				Route::any('/', [GeometryDashProxyAccountDataController::class, 'rejectBase'])->name('base');
+				Route::post('/database/accounts/backupGJAccountNew.php', [GeometryDashProxyAccountDataController::class, 'proxySave'])->name('save');
+				Route::post('/database/accounts/syncGJAccountNew.php', [GeometryDashProxyAccountDataController::class, 'proxyLoad'])->name('load');
+			});
+		});
+
 		Route::post('/{path}', [GeometryDashProxyController::class, 'proxy'])
 			->where('path', '.*')
 			->name('proxy');
