b3dbad0e6a8b9d99873fb593f811c51406ff8749
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-03-27 13:28:28 +0800

    feat(model): add relations

--- app/Models/GeometryDashProxyGameAccountBinding.php
+++ app/Models/GeometryDashProxyGameAccountBinding.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class GeometryDashProxyGameAccountBinding extends Model
 {
 	use HasFactory;
 
 	protected $table = 'gdproxy.game_account_bindings';
+
+	public function data_backups(): HasMany
+	{
+		return $this->hasMany(GeometryDashProxyGameAccountDataBackup::class, 'account_id', 'game_account_id');
+	}
 }
--- app/Models/GeometryDashProxyGameAccountDataBackup.php
+++ app/Models/GeometryDashProxyGameAccountDataBackup.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 
 class GeometryDashProxyGameAccountDataBackup extends Model
 {
 	use HasFactory;
 
 	protected $table = 'gdproxy.game_account_data_backups';
+
+	public function user(): HasOneThrough
+	{
+		return $this->hasOneThrough(User::class, GeometryDashProxyGameAccountBinding::class, 'game_account_id', 'id', 'account_id', 'user_id');
+	}
+
+	public function binding(): HasOne
+	{
+		return $this->hasOne(GeometryDashProxyGameAccountBinding::class, 'game_account_id', 'account_id');
+	}
 }
