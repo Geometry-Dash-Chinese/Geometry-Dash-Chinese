fa96825db820b203f93ca89cafe7077bac813ef6
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-03-27 13:05:47 +0800

    feat(database): add fixed model attribute and fix class import

--- database/factories/GeometryDashProxyGameAccountDataBackupFactory.php
+++ database/factories/GeometryDashProxyGameAccountDataBackupFactory.php
@@ -2,16 +2,19 @@
 
 namespace Database\Factories;
 
-use App\Enums\GeometryDash\Salts;
+use App\Enums\GeometryDashSalts;
+use App\Models\GeometryDashProxyGameAccountDataBackup;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class GeometryDashProxyGameAccountDataBackupFactory extends Factory
 {
+	protected $model = GeometryDashProxyGameAccountDataBackup::class;
+
 	public function definition(): array
 	{
 		return [
 			'account_id' => $this->faker->randomNumber(),
-			'password' => sha1($this->faker->password . Salts::GJP2->value),
+			'password' => sha1($this->faker->password . GeometryDashSalts::GJP2->value),
 			'filename' => $this->faker->file
 		];
 	}
