8e7bda798ef6f417075fc5c539ed9588de6ebfa6
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-03-27 08:51:40 +0800

    feat(provider): extract method

--- app/Providers/GeometryDashChineseCustomizeProvider.php
+++ app/Providers/GeometryDashChineseCustomizeProvider.php
@@ -10,10 +10,19 @@ class GeometryDashChineseCustomizeProvider extends ServiceProvider
 {
 	public function boot(): void
 	{
+		$this->setupAuthenticateRedirect();
+		$this->setupTrustProxies();
+	}
+
+	protected function setupAuthenticateRedirect(): void
+	{
 		Authenticate::redirectUsing(function () {
 			return route('GeometryDashChinese.auth.login');
 		});
+	}
 
+	protected function setupTrustProxies(): void
+	{
 		TrustProxies::at('*');
 	}
 }
