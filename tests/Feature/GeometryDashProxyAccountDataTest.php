7ba5d0e623a3a1f1e17a4d4a4a4d9b2a8223c22d
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-03-27 08:42:01 +0800

    test(GeometryDashProxy): optimize code

--- tests/Feature/GeometryDashProxyAccountDataTest.php
+++ tests/Feature/GeometryDashProxyAccountDataTest.php
@@ -6,29 +6,20 @@ use function Pest\Laravel\post;
 
 [$gameVersion, $binaryVersion] = explode(',', Versions::_22->value);
 
-function getBaseUrl(): string
-{
-	return route('GeometryDashProxy.account.data.base');
-}
-
 test('get url', function () use ($gameVersion, $binaryVersion) {
-	$url = getBaseUrl();
-
 	post(route('GeometryDashProxy.account.url.get'), [
 		'gameVersion' => $gameVersion,
 		'binaryVersion' => $binaryVersion,
 		'type' => AccountDataType::SAVE->value,
 		'accountID' => 71
-	])->assertContent($url);
+	])->assertContent(route('GeometryDashProxy.account.data.base'));
 });
 
 test('get url for load', function () use ($gameVersion, $binaryVersion) {
-	$url = getBaseUrl();
-
 	post(route('GeometryDashProxy.account.url.get'), [
 		'gameVersion' => $gameVersion,
 		'binaryVersion' => $binaryVersion,
 		'type' => AccountDataType::LOAD->value,
 		'accountID' => 71
-	])->assertContent($url);
+	])->assertContent(route('GeometryDashProxy.account.data.base'));
 });
