cf56a7c413b150f4c75d1a6643567f0bdb8ff0a0
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-03-28 17:32:07 +0800

    feat(frontend): add tools homepage for geometry dash proxy

---
+++ resources/components/Pages/GeometryDashProxy/Tools/Home.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+import GeometryDashProxyLayout from '@/components/Layouts/GeometryDashProxy.vue'
+
+export default {
+	layout: GeometryDashProxyLayout
+}
+</script>
+
+<script lang="ts" setup>
+import { Link } from '@inertiajs/vue3'
+import { route } from 'ziggy-js'
+
+const [] = createReusableTemplate<{
+	readonly href: string
+}>()
+</script>
+
+<template>
+	<div class="container mx-auto">
+		<n-flex justify="center">
+			<n-card title="账号工具">
+				<Link :href="route('GeometryDashProxy.tools.account.binding-token.create')">
+					<n-button>创建绑定令牌</n-button>
+				</Link>
+			</n-card>
+		</n-flex>
+	</div>
+</template>
