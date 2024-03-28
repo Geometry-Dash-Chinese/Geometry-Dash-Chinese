391085211d75d878a98e85f0bf8c1f4f16cb154f
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-03-28 17:33:54 +0800

    feat(frontend): add GeometryDashProxyGameAccountBindingToken model type

--- resources/types/models.d.ts
+++ resources/types/models.d.ts
@@ -7,4 +7,13 @@ declare namespace Models {
 		readonly created_at: string
 		readonly updated_at: string
 	}
+
+	export interface GeometryDashProxyGameAccountBindingToken {
+		readonly id: string
+		readonly user_id: number
+		readonly token: string
+		readonly expired_at: string
+		readonly created_at: string
+		readonly updated_at: string
+	}
 }
