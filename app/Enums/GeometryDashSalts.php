453575ddebf248cb1e075296c93014543161a7bb
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-03-28 17:28:15 +0800

    feat(enums): add geometry dash keys and salts

--- app/Enums/GeometryDashSalts.php
+++ app/Enums/GeometryDashSalts.php
@@ -7,8 +7,8 @@ enum GeometryDashSalts: string
 	case LEVEL = 'xI25fpAapCQg';
 	case COMMENT = 'xPT6iUrtws0J';
 	case LIKE_AND_RATE = 'ysg6pUrtjn0J';
-	public const LIKE = GeometryDashSalts::LIKE_AND_RATE;
-	public const RATE = GeometryDashSalts::LIKE_AND_RATE;
+	public const GeometryDashSalts LIKE = GeometryDashSalts::LIKE_AND_RATE;
+	public const GeometryDashSalts RATE = GeometryDashSalts::LIKE_AND_RATE;
 	case USER_PROFILE = 'xI35fsAapCRg';
 	case LEVEL_LEADERBOARD = 'yPg6pUrtWn0J';
 	case CHALLENGE = 'oC36fpYaPtdg';
